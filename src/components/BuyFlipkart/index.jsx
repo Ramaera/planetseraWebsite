bbc9d13cddb0fb0cd8b1891f7bc46d9543fbd970
Owais <owais.it@ramaera.in>
2023-08-31 15:22:28 +0530

    Button responsive

--- src/components/BuyFlipkart/index.jsx
+++ src/components/BuyFlipkart/index.jsx
@@ -5,19 +5,21 @@ const index = ({ link, home }) => {
       target="_blank"
       rel="noopener noreferrer"
       className={`${
-        link && "hover:scale-105 transition-transform"
-      } relative  border-[#1178d0] border-2 p-1 rounded-[50px]`}>
+        link &&
+        "hover:scale-105 transition-transform  border-[#1178d0] border-2 p-1 rounded-[50px]"
+      } relative `}>
       <img
         src="/images/backgrounds/flipkart.webp"
         className={` ${!link && "hover:opacity-10"} z-20  ${
-          !link && "grayscale blur-[1px]"
+          !link &&
+          "grayscale blur-[1px]  border-[#3e3e3e] border-2 p-1 rounded-[50px]"
         }`}
         width={120}
         alt="masala"
       />
       {!link && (
         <h4
-          className={` font-sans font-bold translate-y-1/2 absolute pl-2 top-0 -z-10  text-sm `}>
+          className={` font-sans font-bold absolute top-0 -z-10  text-sm  text-center w-full h-full flex items-center justify-center`}>
           Coming Soon
         </h4>
       )}
