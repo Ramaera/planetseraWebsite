40f30d8ca4e78c287692c22aa7698d5cc3789119
Owais <owais.it@ramaera.in>
2023-09-15 18:24:12 +0530

    Products pade add view button

--- src/app/products/components/RelatedProducts/RelatedProducts.jsx
+++ src/app/products/components/RelatedProducts/RelatedProducts.jsx
@@ -1,13 +1,16 @@
 import RelatedPtoductData from "./RelatedProductData";
 import Link from "next/link";
+import BuynowBtn from "../../../../components/BuynowBtn";
 
 const RelatedProducts = () => {
   return (
     <>
-      <div className="flex flex-wrap mx-2 md:mx-3">
+      <div className="flex flex-wrap  md:mx-auto w-full justify-center mb-4">
         {RelatedPtoductData.map((pro) => {
           return (
-            <div className="w-2/4 md:w-1/4 p-1 md:p-4" key={pro.id}>
+            <div
+              className="w-[44%] md:w-[23%]	 p-2 md:p-4 border-gray-200 border-[1px] rounded-2xl my-2 mx-1 md:m-2"
+              key={pro.id}>
               <Link
                 href={`/products/${pro.id}`}
                 // onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
@@ -18,7 +21,7 @@ const RelatedProducts = () => {
                   title={`Buy Planetsera ${pro.ProductName}`}
                 />
                 <h2
-                  className="text-center py-2 md:text-xl	text-slate-600"
+                  className="text-center py-2 text-[13px] md:text-xl	text-slate-600"
                   // style={{
                   //   color: pro.colored,
                   // }}
@@ -26,6 +29,13 @@ const RelatedProducts = () => {
                   {pro.ProductName}
                 </h2>
               </Link>
+              <div className="mt-[-1rem]">
+                <BuynowBtn
+                  link={`/products/${pro.id}`}
+                  text={"View"}
+                  sectionClass="responsiveBtn"
+                />
+              </div>
             </div>
           );
         })}
