a19ce60939edd2ef3ab86680bce8587b9f217b49
Owais <owais.it@ramaera.in>
2023-09-18 12:09:38 +0530

    Product align

--- src/app/products/components/RelatedProducts/RelatedProducts.jsx
+++ src/app/products/components/RelatedProducts/RelatedProducts.jsx
@@ -5,37 +5,37 @@ import BuynowBtn from "../../../../components/BuynowBtn";
 const RelatedProducts = () => {
   return (
     <>
-      <div className="flex flex-wrap  md:mx-auto w-full justify-center mb-4">
+      <div className="flex flex-wrap mx-2 md:mx-3 mb-4">
         {RelatedPtoductData.map((pro) => {
           return (
-            <div
-              className="w-[44%] md:w-[23%]	 p-2 md:p-4 border-gray-200 border-[1px] rounded-2xl my-2 mx-1 md:m-2"
-              key={pro.id}>
+            <div className="w-2/4 md:w-1/4 p-1 md:p-3" key={pro.id}>
               <Link
                 href={`/products/${pro.id}`}
                 // onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
               >
-                <img
-                  src={pro.RelatedPtoductImg}
-                  alt={pro.ProductName}
-                  title={`Buy Planetsera ${pro.ProductName}`}
-                />
-                <h2
-                  className="text-center py-2 text-[13px] md:text-xl	text-slate-600"
-                  // style={{
-                  //   color: pro.colored,
-                  // }}
-                >
-                  {pro.ProductName}
-                </h2>
+                <div className="border-gray-200 border-[1px] rounded-2xl px-3 pt-3 pb-1">
+                  <img
+                    src={pro.RelatedPtoductImg}
+                    alt={pro.ProductName}
+                    title={`Buy Planetsera ${pro.ProductName}`}
+                  />
+                  <h2
+                    className="text-center py-2 text-[13px] md:text-xl	text-slate-600"
+                    // style={{
+                    //   color: pro.colored,
+                    // }}
+                  >
+                    {pro.ProductName}
+                  </h2>
+                  <div className="mt-[-1rem]">
+                    <BuynowBtn
+                      link={`/products/${pro.id}`}
+                      text={"View"}
+                      sectionClass="responsiveBtn"
+                    />
+                  </div>
+                </div>
               </Link>
-              <div className="mt-[-1rem]">
-                <BuynowBtn
-                  link={`/products/${pro.id}`}
-                  text={"View"}
-                  sectionClass="responsiveBtn"
-                />
-              </div>
             </div>
           );
         })}
